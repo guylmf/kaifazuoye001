15568a647926d374f5d626538b08d86e50217fb9
guylmf <1300387653@qq.com>
2015-12-10 16:10:20 +0800

    qq.002第二次修改推送，主要是js方面

--- qq首页/qq.js
+++ qq首页/qq.js
@@ -1,10 +1,49 @@
+  
+  window.onload = function(){
+  	var aUl = document.getElementById('albumWrap');
+  	var allLi = aUl.getElementsByTagName('li');
+  	for(var i=0; i<allLi.length; i++){
+  		allLi[i].onmouseover = function(){
+  			this.getElementsByTagName('a')[1].style.display = 'block';
+  		}
+  		allLi[i].onmouseout = function(){
+  			this.getElementsByTagName('a')[1].style.display = 'none';
+  		}
+  }
 
+  	var topNav = document.getElementsByClassName('topNav')[0];
+  	var navs = topNav.getElementsByTagName('li');
+  	for(var i=0; i<navs.length; i++){
+  		navs[i].onmouseover = function(){
+  			topNav.getElementsByClassName('current')[0].className = '';
+  			this.className = 'current';
+  		}
+  		navs[i].onmouseout = function(){
+  			topNav.getElementsByClassName('current')[0].className = '';
+  			navs[2].className = 'current';
+  		}
+      
+  	}
 
-function onmouse(){    
-   if(document.getElementById("albumWrap").getElementsByTagname("a").style.display=="none"){
-      document.getElementById("albumWrap").getElementsByClassname("alcover").style.opacity=="0.7";
-   }
-   else if(document.getElementById("albumWrap").getElementsByTagname("a").style.display=="block"){
-      document.getElementById("albumWrap").getElementsByTagname("a").style.opacity=="0.4";
-   }
-}
+  var pic=document.getElementById('crossbanenr').getElementsByTagName('li'),
+        index=1,
+        timer=null;
+
+    timer = setInterval(autoPlay, 4000);
+
+     
+    function autoPlay () {
+      if (++index >= pic.length) index = 0;
+      changePic(index);
+    }
+
+ 
+    function changePic (curIndex) {
+      for (var i = 0; i < pic.length; ++i) {
+        pic[i].style.display = "none";
+       
+      }
+      pic[curIndex].style.display = "block";
+     
+    }
+  }
